8589ee34eaccd05038918be6e200853c45cfb322
iproyak-cmd <iproyak@gmail.com>
2026-07-20 19:22:28 +0300

    Fix Fabula companion imports

--- lib/fabula_companion.dart
+++ lib/fabula_companion.dart
@@ -1,4 +1,5 @@
-// Fabula companion client; OpenRouter credentials stay on the server.\nimport 'dart:convert';
+// Fabula companion client; OpenRouter credentials stay on the server.
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
